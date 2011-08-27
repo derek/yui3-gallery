202ad001dcd5fb5a2952b08211753de2b2e80c8a
Derek Gathright <drgath@gmail.com>
2011-08-26 18:53:26 -0700

    moved css to skins, added a night skin, changed some path locations, updated build properties.

--- src/gallery-prettify/fetch.sh
+++ src/gallery-prettify/fetch.sh
@@ -6,10 +6,12 @@ mkdir {js,css,src}
 #Fetch the files
 curl "http://google-code-prettify.googlecode.com/svn/trunk/src/prettify.js" -o src/prettify.js
 curl "http://google-code-prettify.googlecode.com/svn/trunk/src/prettify.css" -o src/prettify.css
+curl "http://google-code-prettify.googlecode.com/svn/trunk/styles/sunburst.css" -o src/sunburst.css
 
 #Process and move the files
 cat src/prettify.js | sed 's/window\[/Y\[/g' > js/prettify.js
-cp src/prettify.css css/prettify.css
+cp src/prettify.css assets/skins/sam/gallery-prettify-skin.css
+cp src/sunburst.css assets/skins/night/gallery-prettify-skin.css
 
 #Clean up
 rm -rf src
